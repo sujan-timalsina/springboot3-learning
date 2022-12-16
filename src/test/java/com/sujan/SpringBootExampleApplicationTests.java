f5b52dfc628124fec4053e14e10d734c83f0fa8c
sujan-timalsina <sujan.timalsina@apexcollege.edu.np>
2022-12-16 13:34:00 +0545

    starter template

---
+++ src/test/java/com/sujan/SpringBootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sujan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
