e2028a42b7782fdce93a16a4ebfafbc971deb416
sujan-timalsina <sujan.timalsina@apexcollege.edu.np>
2022-12-19 17:12:44 +0545

    simple crud

--- src/main/java/com/sujan/Main.java
+++ src/main/java/com/sujan/Main.java
@@ -2,15 +2,22 @@ package com.sujan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Objects;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
@@ -72,6 +79,45 @@ public class Main {
         }
     }
     */
+
+    @GetMapping
+    public List<Customer> getCustomers(){
+        return customerRepository.findAll();
+    }
+
+    record NewCustomerRequest(
+            String name,
+            String email,
+            Integer age
+    ){
+
+    }
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request){
+        Customer customer = new Customer();
+        customer.setAge(request.age);
+        customer.setEmail(request.email);
+        customer.setName(request.name);
+        customerRepository.save(customer);
+    }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id){
+        customerRepository.deleteById(id);
+    }
+
+    @PutMapping("{customerId}")
+    public void updateCustomer(@PathVariable("customerId") Integer id, @RequestBody NewCustomerRequest request){
+        Customer updateCustomer = customerRepository.findById(id)
+                .orElseThrow(() -> new ArithmeticException("Employee not exist with id: " + id));
+                // .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
+
+        updateCustomer.setAge(request.age);
+        updateCustomer.setEmail(request.email);
+        updateCustomer.setName(request.name);
+        customerRepository.save(updateCustomer);
+    }
 }
 
 /*
